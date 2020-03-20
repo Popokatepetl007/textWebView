a67a5dfed0808fba0c7f5cf20dce1ba2a0df1da8
Popokatepetl007 <temik007@mail.ru>
2020-03-20 18:05:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/textwebview/MainActivity.kt
@@ -0,0 +1,267 @@
+package com.example.textwebview
+
+//import sun.jvm.hotspot.utilities.IntArray
+
+//import sun.jvm.hotspot.utilities.IntArray
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.os.Parcelable
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.*
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+//import sun.jvm.hotspot.utilities.IntArray
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    var imageUri: Uri? = null
+
+    private var mUploadMessage: ValueCallback<Uri>? = null
+    private var mCapturedImageURI: Uri? = null
+    private var mFilePathCallback: ValueCallback<Array<Uri>>? = null
+    private var mCameraPhotoPath: String? = null
+    private val INPUT_FILE_REQUEST_CODE = 1
+    private val FILECHOOSER_RESULTCODE = 1
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    val permissions = arrayOf(android.Manifest.permission.CAMERA,  android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private fun hasNoPermissions(): Boolean{
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermission(){
+        ActivityCompat.requestPermissions(this, permissions,0)
+    }
+
+    @Throws(IOException::class)
+    open fun createImageFile(): File? {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES
+        )
+        return File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webSettings = webCont.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        webCont.scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+        webCont.isScrollbarFadingEnabled = false
+        webSettings.builtInZoomControls = true
+        webSettings.displayZoomControls = false
+        webSettings.setSupportZoom(true)
+        webSettings.defaultTextEncodingName = "utf-8"
+        webSettings.pluginState = WebSettings.PluginState.ON
+        webSettings.allowFileAccess = true
+        webSettings.setSupportZoom(true)
+
+        if(hasNoPermissions()){
+            requestPermission()
+        }else {
+            webCont.loadUrl("https://livetex.vladlink.ru/?uid=128311")
+            setClients()
+        }
+    }
+
+    private fun setClients() {
+        webCont.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {}
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+            }
+
+        }
+
+        webCont.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowFileChooser(
+                view: WebView?,
+                filePath: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                mFilePathCallback?.onReceiveValue(null)
+                mFilePathCallback = filePath
+                var takePictureIntent: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (takePictureIntent!!.resolveActivity(packageManager) != null) {
+                    var photoFile: File? = null
+                    try {
+                        photoFile = createImageFile()
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath)
+                    } catch (ex: IOException) {
+                        Log.e("logFromChrom", "Pizda in File", ex)
+                    }
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.absolutePath
+                        takePictureIntent.putExtra(
+                            MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photoFile)
+                        )
+                    } else {
+                        takePictureIntent = null
+                    }
+                }
+                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                contentSelectionIntent.type = "image/*"
+                val intentArray: Array<Intent?>
+                intentArray = takePictureIntent.let { arrayOf(it) } ?: arrayOfNulls<Intent>(0)
+                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
+                startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE)
+                return true
+            }
+
+
+            fun openFileChooser(
+                uploadMsg: ValueCallback<Uri>?,
+                acceptType: String?
+            ) {
+                mUploadMessage = uploadMsg
+
+                val imageStorageDir = File(
+                    Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES
+                    )
+                    , "AndroidExampleFolder"
+                )
+                if (!imageStorageDir.exists()) {
+                    imageStorageDir.mkdirs()
+                }
+
+                val file = File(
+                    imageStorageDir.toString() + File.separator + "IMG_"
+                            + System.currentTimeMillis().toString() + ".jpg"
+                )
+                mCapturedImageURI = Uri.fromFile(file)
+
+                val captureIntent = Intent(
+                    MediaStore.ACTION_IMAGE_CAPTURE
+                )
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI)
+                val i = Intent(Intent.ACTION_GET_CONTENT)
+                i.addCategory(Intent.CATEGORY_OPENABLE)
+                i.type = "image/*"
+
+                val chooserIntent = Intent.createChooser(i, "Image Chooser")
+
+                chooserIntent.putExtra(
+                    Intent.EXTRA_INITIAL_INTENTS
+                    , arrayOf<Parcelable>(captureIntent)
+                )
+
+                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE)
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
+                super.onActivityResult(requestCode, resultCode, data)
+                return
+            }
+            var results: Array<Uri>? = null
+
+            if (resultCode == Activity.RESULT_OK) {
+                if (data == null) {
+                    if (mCameraPhotoPath != null) {
+                        results = arrayOf(Uri.parse(mCameraPhotoPath))
+                    }
+                } else {
+                    val dataString = data.dataString
+                    if (dataString != null) {
+                        results = arrayOf(Uri.parse(dataString))
+                    }
+                }
+            }
+            mFilePathCallback!!.onReceiveValue(results)
+            mFilePathCallback = null
+        } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            if (requestCode != FILECHOOSER_RESULTCODE || mUploadMessage == null) {
+                super.onActivityResult(requestCode, resultCode, data)
+                return
+            }
+            if (requestCode == FILECHOOSER_RESULTCODE) {
+                if (null == mUploadMessage) {
+                    return
+                }
+                var result: Uri? = null
+                try {
+                    result = if (resultCode != Activity.RESULT_OK) {
+                        null
+                    } else {
+                        if (data == null) mCapturedImageURI else data.data
+                    }
+                } catch (e: java.lang.Exception) {
+                    Toast.makeText(
+                        applicationContext, "activity :$e",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                mUploadMessage!!.onReceiveValue(result)
+                mUploadMessage = null
+            }
+        }
+
+        return
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        webCont.loadUrl("https://livetex.vladlink.ru/?uid=128311")
+        setClients()
+    }
+
+}
+
+
